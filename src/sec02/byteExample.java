86536977689307b74d78d8ee3afc1d75b8e9cdf9
Shu-wep <leeshu.zip@gmail.com>
2024-01-24 13:54:08 +0900

    강의 챕터02

---
+++ src/sec02/byteExample.java
@@ -0,0 +1,14 @@
+package sec02;
+
+public class byteExample {
+		public static void main(String[] args) {
+			byte var1 = -128;
+//			byte var2 = 128; // byte는 -128 ~ 127까지 대입가능
+			
+			System.out.println("var1=" + var1);
+			
+			
+			
+	
+		}
+}
