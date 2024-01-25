9a57b2d12258f382eeee738b0cae24bc223f3995
Siwoo Lee <leeshu.zip@gmail.com>
2024-01-25 21:23:00 +0900

    챕터13

--- Chap13.Exception/src/sec04_printStackTrace_getMessage/PrintStackTrace.java
+++ Chap13.Exception/src/sec04_printStackTrace_getMessage/PrintStackTrace.java
@@ -1,10 +1,13 @@
-package sec03_excepyion_process;
+package sec04_printStackTrace_getMessage;
 
-public class TryCatchEx2 {
-	/*
-	 * try-catch 흐름
-	 */
+public class PrintStackTrace {
+
 	public static void main(String[] args) {
+		
+		//예외가 발생되면 메서드를 통해 내용을 가져올수있다.
+		//printStackTrace() 호출스택에 있었떤 메서드의 정보와 예외 메시지 출력
+		//getMessage() 발생한 예외클래스의 인스턴스에 저장된 메시지를 얻을 수 있음.
+		
 		System.out.println("1");
 		System.out.println("2");
 
@@ -14,12 +17,16 @@ public class TryCatchEx2 {
 			System.out.println("4");
 		} catch (ArithmeticException ae) {
 			if (ae instanceof ArithmeticException)
+				ae.printStackTrace();
+				System.out.println(ae.getMessage());
 				System.out.println("true");
 			System.out.println("ArithmeticException");
 		} catch (Exception a) {
 			System.out.println("Exception");//모든 예외의 최고조상 Exception
 		}
 		System.out.println("6");
+
+		
 
 	}
 
